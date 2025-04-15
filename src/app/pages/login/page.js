0df82eea8152d679d49192620a8794c61aa6c819
Aidan Garton <amgb2018@mymail.pomona.edu>
2025-04-15 17:45:46 -0400

    added login page

---
+++ src/app/pages/login/page.js
@@ -0,0 +1,72 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+const users = [
+  { username: "user1", password: "password1", isAdmin: false },
+  { username: "admin1", password: "adminpass1", isAdmin: true },
+];
+
+export default function LoginPage() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const router = useRouter();
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const user = users.find(
+      (user) => user.username === username && user.password === password
+    );
+    if (user) {
+      // Store user info in local storage or context
+      // localStorage.setItem("user", JSON.stringify(user));
+
+      // Redirect user based on role
+      if (user.isAdmin) {
+        router.push("/admin");
+      } else {
+        router.push("/dashboard");
+      }
+    } else {
+      setError("Invalid username or password");
+    }
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100 px-4">
+      <div className="w-full max-w-md bg-white p-8 rounded-xl shadow-lg">
+        <h1 className="text-2xl font-bold mb-6 text-center">Login</h1>
+
+        <form onSubmit={handleLogin} className="space-y-4">
+          <label className="block text-sm font-medium text-gray-700 mb-1">
+            Username:{" "}
+          </label>
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            className="w-full border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 mb4" //mb-4
+          ></input>
+
+          <label>Password: </label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="w-full border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          ></input>
+
+          <button
+            type="submit"
+            className="w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 transition-colors font medium"
+          >
+            Login
+          </button>
+          {error && <p className="text-red-500 text-sm">{error}</p>}
+        </form>
+      </div>
+    </div>
+  );
+}
